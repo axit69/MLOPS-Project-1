604330c56f48dad5b2da1c6a09316c1d27cac578
axitmehta <axitmehta6@gmail.com>
2025-09-04 21:26:49 +0530

    project structure ready

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="Axit Mehta",
+    author_email="axitmehta69@gmail.com",
+    packages=find_packages()
+)
